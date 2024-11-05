5b375e28b1a001c9b2dece61b367bce29a40b350
Marcelo Torres <marceloignacio.torres@alumnos.ulagos.cl>
2024-11-05 09:37:06 -0300

    Página de e-commerce
    ver 0.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
-import HeaderComponent from './components/Header/Header';
-import FooterComponent from './components/Footer/Footer';
+import HeaderComponent from './components/Header/Header.jsx';
+import FooterComponent from './components/Footer/Footer.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './components/Home/Home';
-import AboutPage from './components/About/About';
-import ContactPage from './components/Contact/Contact';
+import HomePage from './components/Home/Home.jsx';
+import AboutPage from './components/About/About.jsx';
+import ContactPage from './components/Contact/Contact.jsx';
 import ProductsPage from './Components/Productos/Productos.jsx';
-import ItemDetail from './components/ItemDetail/ItemDetail';
-import ProductsCategoria from './components/Categoria/Categoria';
+import ItemDetail from './components/ItemDetail/ItemDetail.jsx';
+import ProductsCategoria from './components/Categoria/Categoria.jsx';
 
 function App() {
     return (
